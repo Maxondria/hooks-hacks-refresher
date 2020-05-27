02c24af3d31d5f2e9d9ec18bce5e0cf39255b401
Maxondria <maxtayebwa@gmail.com>
2020-05-27 20:28:31 +0300

    add useFetch hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "./custom/useForm";
+import { useFetch } from "./custom/useFetch";
 
 const App = () => {
   const [{ email, password }, handleChange] = useForm({
     email: "",
     password: "",
   });
+
+  const [count, setCount] = useState(0);
+
+  const [data, loading] = useFetch(`http://numbersapi.com/${count}/trivia`);
+
+  // useEffect(() => {
+  //   const onMouseMove = (e) => console.log(e);
+  //   window.addEventListener("mousemove", onMouseMove);
+
+  //   return () => {
+  //     window.removeEventListener("mousemove", onMouseMove);
+  //   };
+  // }, []);
+
   return (
     <div>
+      <div>{loading ? "Loading..." : data}</div>
+      <div>Count: {count}</div>
+      <button onClick={() => setCount((c) => c + 1)}>Increment</button>
       <input
         type="email"
         value={email}
